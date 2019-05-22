9cea9098eca3bcfca668f267b139332cd91b7be3
Bihengoto <isaacbihengoto@gmail.com>
2019-05-22 03:40:51 +0300

    included the code for the petnamedetails that had butterknife functionality with the respective code forandroid manifest and finally I rectified a bug in the xml file

--- app/src/main/java/com/mahugu/petfinder/PetNameDetails.java
+++ app/src/main/java/com/mahugu/petfinder/PetNameDetails.java
@@ -1,13 +1,43 @@
 package com.mahugu.petfinder;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class PetNameDetails extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class PetNameDetails extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.find)
+    Button mfind;
+    @BindView(R.id.locationEditText)
+    EditText mLocationEditText;
+    @BindView(R.id.appNameTextView)
+    TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pet_name_details);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mfind.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        if(v == mfind){
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(PetNameDetails.this, PetsInfo.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/mahugu/petfinder/PetsInfo.java
@@ -0,0 +1,13 @@
+package com.mahugu.petfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PetsInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pets_info);
+    }
+}
